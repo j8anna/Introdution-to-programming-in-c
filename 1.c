1b0391dcba288d455424d077f256a9e9a410a241
Joanna Górecka <j8anna@users.noreply.github.com>
2017-11-06 07:20:39 +0100

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+/* Wypisz na ekranie swoje imiê i nazwisko, a nastêpnie wydrukuj kilka informacji o sobie w postaci wizytówki. 
+Wykorzystaj tabulatory i podzia³ na wiersze. */
+
+#include <stdio.h>
+int main(void) {
+
+printf("Imie i nazwisko\n");
+printf("Studentka\tUczelni\nKierunek\tInformatyka\nRok\tI\nStudia\t dzienne\n");
+return 0;
+
+// /v Dziala od ANSI C (tabulator pionowy).
+}
