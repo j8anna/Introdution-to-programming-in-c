2ea26e113ffef2df9cc069943ea3b2166282ed2b
Joanna Górecka <j8anna@users.noreply.github.com>
2018-02-12 09:57:52 +0100

    Delete 6.c

--- 6.c
+++
@@ -1,9 +0,0 @@
-// Wpisz dowolny znak i wyœwietl jego kod.
-#include <stdio.h>
-int main(void) {
-char znak;
-printf("Podaj dowolny znak\n");
-scanf("%c", &znak);
-printf("Twoj znak ma numer %d.", znak);
-	return 0;
-}
